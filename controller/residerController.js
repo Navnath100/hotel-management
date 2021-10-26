9d704ed9944f4632aa657030681e8c07a1da3418
Navnath <navnathphapale100@gmail.com>
2021-10-26 14:13:29 +0530

    add resider & check in

--- controller/residerController.js
+++ controller/residerController.js
@@ -10,7 +10,8 @@ const { upload,s3 } = require('../middlewares/uploadImg')
 const sendSMS = require('../middlewares/sms')
 let residerCount = null;
 if (residerCount == null) {
-        Resider.find().countDocuments().then(val =>residerCount=val+1);
+    console.log("residerCount is null");
+        Resider.find().countDocuments().then(val =>residerCount=val+1).catch(err =>console.log(err););
 }
 // /api/resider/
 async function getResiders(req,res,next) {
