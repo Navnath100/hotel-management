2dcec7167ceb82b65c98e473acc30de7cd05c18e
100Navnath <100navnathphapale@gmail.com>
2021-11-23 09:35:49 +0530

    todayBusiness

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -1,5 +1,6 @@
 const joi=require('joi')
 const { User } = require('../models/users')
+const { Resider } = require('../models/residers')
 const { Transaction } = require('../models/transactions')
 const { sendEmail } = require('../middlewares/notification');
 const hashPassword = require('password-hash');
@@ -299,29 +300,120 @@ async function withdrawal(req,res,next) {
 //api/resider//today-business/:id
 async function todayBusiness(req,res,next) {
     try {
-            User.findOne({_id : req.params.id}).then(user =>{
+            User.findOne({_id : req.params.id}).then(async(user) =>{
             if(user && user.status == "Active"){
                 const now = new Date();
                 const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+                const { date_filter } = req.query;
+                const search = {};
                 const client = {}
 
-                Transaction.find({createdAt: {$gte: today}}).then(today_business=>{
-                    if(today_business.length > 0){
-                        let totalAmount = 0;
-                        for (let i = 0; i < today_business.length; i++) {
-                            if(today_business[i].type == "credit" && today_business[i].transactionFor != "Withdrew")
-                                 totalAmount += today_business[i].amount                       
-                            else if(today_business[i].type == "debit" && today_business[i].transactionFor != "Withdrew")
-                                 totalAmount -= today_business[i].amount
-                            }
-                        client["todayBusiness"] = totalAmount;
-                        
-                        // res.json(client);
-                        res.json(client);
-                    }else
-                        res.json({error:"Entries not found for today."})   
-                });
-            }else
+
+        if(date_filter == "today"){
+            search.createdAt = {
+                $gte: new Date(new Date().setHours(00, 00, 00)),
+                $lt: new Date(new Date().setHours(23, 59, 59))
+            }
+        }
+
+        const dt = new Date();
+        const day = dt.getDay();
+        let n = null; // last Monday conversion
+
+        switch (dt.getDay()) {
+            case 0: n = -6; break;
+            case 1: n = 0; break;
+            case 2: n = -1; break;
+            case 3: n = -2; break;
+            case 4: n = -3; break;
+            case 5: n = -4; break;
+            case 6: n = -5; break;
+            default: "This never happens";
+        }
+
+        const monday = new Date().setDate(new Date().getDate() + n );
+        const sunday = new Date().setDate(new Date().getDate() + 6 );
+
+        if(date_filter == "this-week"){
+            search.createdAt = {
+                $gte: new Date(monday).setHours(00, 00, 00),
+                $lt: new Date()
+            }
+        }
+
+        if(date_filter == "last-week"){
+            const lastWeekMonday = new Date().setDate((new Date().getDate() + n)-7 );
+            const lastWeeksunday = new Date().setDate(new Date().getDate()+n-1);
+            search.createdAt = {
+                $gte: new Date(lastWeekMonday).setHours(00, 00, 00),
+                $lt: new Date(lastWeeksunday).setHours(23, 59, 59)
+            }
+        }
+
+        if(date_filter == "this-month"){
+            // const year = ff;
+            const daysInMonth = new Date(new Date().getFullYear(), new Date().getMonth()+1, 0).getDate();
+            const firstDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth());
+            const lastDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(),daysInMonth);
+            search.createdAt = {
+                $gte: new Date(firstDayOfMonth).setHours(00, 00, 00),
+                $lt: new Date(lastDayOfMonth).setHours(23, 59, 59)
+            }
+        }
+
+        if(date_filter == "last-month"){
+            const daysInMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 0).getDate();
+            const firstDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth()-1);
+            const lastDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth()-1,daysInMonth);
+            search.createdAt = {
+                $gte: new Date(firstDayOfMonth).setHours(00, 00, 00),
+                $lt: new Date(lastDayOfMonth).setHours(23, 59, 59)
+            }
+        }
+
+        if(date_filter == "last-three-months"){
+            const currentDate = new Date();
+            const year = currentDate.getFullYear();
+            const month = currentDate.getMonth()+1;
+            const daysInMonth = new Date(year, month, 0).getDate();
+            const firstDayOfMonth = new Date(year+"-"+(month-3)+"-"+1);
+            const lastDayOfMonth = new Date(year+"-"+month+"-"+currentDate.getDate());
+            search.createdAt = {
+                $gte: new Date(firstDayOfMonth).setHours(00, 00, 00),
+                $lt: new Date(lastDayOfMonth).setHours(23, 59, 59)
+            }
+        }
+        
+        if(date_filter == "this-year"){
+            const currentDate = new Date();
+            const year = currentDate.getFullYear();
+            const firstDayOfYear = new Date(year+"-"+1+"-"+1);
+            const lastDayOfYear = new Date(year+"-"+12+"-"+31);
+            search.createdAt = {
+                $gte: new Date(firstDayOfYear).setHours(00, 00, 00),
+                $lt: new Date(lastDayOfYear).setHours(23, 59, 59)
+            }
+        }
+        client["AC_nonAC"] = await Resider.aggregate([ 
+            {$match: {$and:[
+                {$nor: [ { status: "Pending" } ]},
+                {$or: [ 
+                    search, // uncomment this to get dynamic result according to date_filter
+                    
+                    {updatedAt : {$gte: new Date(new Date().setHours(00, 00, 00)),$lt: new Date(new Date().setHours(23, 59, 59))}}
+                    ]}
+                ]}},
+            {$group: { 
+                "_id": '$isAC',
+                // "_id": { "$first": "$isAC" },  //$first accumulator
+                Advance:{$sum:'$advance'},
+                StayAmount:{$sum:'$bill.StayCharges'},
+                count: { $sum: 1 }
+             }} 
+            ])
+            res.json(client);
+            
+        }else
                 return next(new Error("Unauthorized access denied"))
         }).catch(err=>{
             return next(new Error(err))
